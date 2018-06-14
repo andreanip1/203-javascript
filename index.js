716cd834fbff51677f5b30f824bd8e538a0af59f
Priscila Andreani <priscilaandreani@gmail.com>
2018-06-14 11:57:28 -0300

    PRIMEIRA AULA

---
+++ index.js
@@ -0,0 +1,76 @@
+/*
+String: "Nome" 'Nome' `Nome`; 
+Number:  50 ; 
+Boolean: true or false; 
+
+Operações: 
+15 + 4 = 19
+15/4 = 3.75
+10 * 2 = 20
+typeof(4.8247): number
+
+Concatenar: 
+"Renan"+"Rodrigo" = "RenanRodrigo"
+"Renan" - "Rodrigo" = NaN
+NaN = not a number
+null:  nulo 
+"Renan".toUpperCase = "RENAN"
+"Renan".repeat(2) = "RenanRenan"
+
+Booleano
+true +  true = 2
+!:  not (prioridade)
+!true: false
+
+true && true: true
+true && false: false
+false & true:  false
+false & false: false
+true & true & true &  true: true
+true & true & false: false
+
+|| = ou
+true || true: true
+true || false: true
+false || true: true
+true || true || false || true || false: true
+
+true || false && true:  true
+true && false || true: true
+true && (false || false): false
+true && (false || !(false || true || false) && true): false
+
+Comparações (o resultado de uma comparação é sempre booleano)
+10 === 5: false
+"Renan" === "Renan": true
+10/2 === 5: true
+10/2 !== 5: false 
+5 > 2: true 
+2 > 5: true
+3 < 1: false
+>= maior ou igual 
+<= menor ou igual
+
+number(60): 60
+number("60"): 60
+number(true): 1 != number ("true"): NaN
+number("six"): NaN
+
+string("Renan"):  Renan
+string("1234"):  "1234"
+string("true"): "true"
+
+boolean(true): true
+boolean(60): true
+boolean(0): false
+boolean(-15): true
+boolean(""): false
+
+Variáveis 
+let (criar a variável)
+let = (definir a variável)
+
+Constante (coisas que nunca vão mudar)
+const 
+const pi = 3.1415
+*/
